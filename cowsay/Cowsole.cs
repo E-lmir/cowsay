48985bccab24410743f0d08316a0c90d32a27a1f
E-lmir <mollymaloun@gmail.com>
2024-10-09 21:10:02 +0400

    Add project files.

---
+++ cowsay/Cowsole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using cowsaу;
+
+namespace cowsay
+{
+    internal static class Cowsole
+    {
+        internal static void WriteLine(string message) => Console.WriteLine(message);
+        internal static void Write(string message) => Console.Write(message);
+    }
+}
---
+++ cowsay/Program.cs
@@ -0,0 +1,29 @@
+﻿using cowsaу;
+
+namespace cowsay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cowMessageLeftBorder = "| ";
+            var cowMessageRightBorder = " |";
+            var cowMessage = cowMessageLeftBorder + (args.Length > 0 ? args[0] : " ") + cowMessageRightBorder;
+            var cowMessageLine = "+" + new string('-', cowMessage.Length - 2) + "+";
+
+            var cow = @"
+    \
+     \ ^__^
+      \(oo)\________
+       (__)\        )\/\
+           ||-----w |
+           ||      ||
+            ";
+
+            Cowsole.WriteLine(cowMessageLine);
+            Cowsolе.WriteLine(cowMessage);
+            Cowsole.Write(cowMessageLine);
+            Cowsole.WriteLine(cow);
+        }
+    }
+}
