a14fce9b6d819979947db6ccf3bf5bca46160c93
E-lmir <mollymaloun@gmail.com>
2024-10-31 23:23:09 +0400

    Add jokes api

--- cowsay/Program.cs
+++ cowsay/Program.cs
@@ -1,17 +1,13 @@
-﻿using cowsaу;
+﻿using System.Net.Http.Json;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json;
 
 namespace cowsay
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            var cowMessageLeftBorder = "| ";
-            var cowMessageRightBorder = " |";
-            var cowMessage = cowMessageLeftBorder + (args.Length > 0 ? args[0] : " ") + cowMessageRightBorder;
-            var cowMessageLine = "+" + new string('-', cowMessage.Length - 2) + "+";
-
-            var cow = @"
+        private static HttpClient _httpClient => new HttpClient();
+        private const string Cow = @"
     \
      \ ^__^
       \(oo)\________
@@ -20,10 +16,43 @@ namespace cowsay
            ||      ||
             ";
 
-            Cowsole.WriteLine(cowMessageLine);
-            Cowsolе.WriteLine(cowMessage);
-            Cowsole.Write(cowMessageLine);
-            Cowsole.WriteLine(cow);
+        static async Task Main(string[] args)
+        {
+            var isLooped = false;
+            if (args[0].StartsWith("-t:"))
+            {
+                isLooped = true;
+                args[0] = string.Empty;
+            }
+
+            var period = isLooped ? int.Parse(args[0].Split(':')[1]) * 1000 : 0;
+            
+            do
+            {
+                var cowMessageLeftBorder = "| ";
+                var cowMessageRightBorder = " |";
+                var cowMessage = cowMessageLeftBorder + (args[0]?.Length > 0 ? args[0] : await GetJoke()) + cowMessageRightBorder;
+                var cowMessageLine = "+" + new string('-', cowMessage.Length - 2) + "+";
+                await Task.Delay(period);
+
+                Cowsole.WriteLine(cowMessageLine);
+                Cowsole.WriteLine(cowMessage);
+                Cowsole.Write(cowMessageLine);
+                Cowsole.WriteLine(Cow);               
+            }
+            while (isLooped);
         }
+
+
+        private static async Task<string> GetJoke()
+        {
+            var response = await _httpClient.GetAsync("https://geek-jokes.sameerkumar.website/api?format=json");
+            var json = JsonSerializer.Deserialize<Joke>(await response.Content.ReadAsStringAsync());
+
+            return json.joke;
+        }
+
+        public record Joke(string joke);
     }
+
 }
